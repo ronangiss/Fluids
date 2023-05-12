5a63533561de3a30dd4ccbb31cd5aeb1a79d279e
ronangiss <46977169+ronangiss@users.noreply.github.com>
2023-05-12 15:55:45 -0400

    latex rather than mathml

--- equation_highlight.js
+++ equation_highlight.js
@@ -1,29 +1,23 @@
 // Show hidden DIV on hover
-document.addEventListener('mouseover', function handleMouseOver(e) {
-	if (e.target.closest(".equation_left")){
-		equation = e.target.closest(".equation_left").parentElement.parentElement.parentElement;
-		equation.querySelector('.equation_left_hidden').style.visibility = 'visible';
-	 	e.target.closest(".equation_left").style.backgroundColor = "lightgray";
-	}
-	if (e.target.closest(".equation_right")){
-		equation = e.target.closest(".equation_right").parentElement.parentElement.parentElement;
-        equation.querySelector('.equation_label').style.visibility = 'hidden';
-	 	equation.querySelector('.equation_right_hidden').style.visibility = 'visible';
-	 	e.target.closest(".equation_right").style.backgroundColor = "lightgray";
-	}
+$(".equation_left").mouseenter(function(){
+    $(this).parent().siblings(".equation_left_hidden").css("visibility", "visible");
+    $(this).css("background-color", "lightgray");
+});
+
+$(".equation_right").mouseenter(function(){
+    $(this).parent().siblings(".equation_right_hidden").css("visibility", "visible");
+    $(this).css("background-color", "lightgray");
+    $(this).parent().siblings(".equation_label").css("visibility", "hidden");
 });
 
 // Hide DIV on mouse out
-document.addEventListener('mouseout', function handleMouseOut(e) {
-	if (e.target.closest(".equation_left")){
-		equation = e.target.closest(".equation_left").parentElement.parentElement.parentElement;
-		equation.querySelector('.equation_left_hidden').style.visibility = 'hidden';
-		e.target.closest(".equation_left").style.backgroundColor = "transparent";
-	}
-	if (e.target.closest(".equation_right")){
-		equation = e.target.closest(".equation_right").parentElement.parentElement.parentElement;
-	 	equation.querySelector('.equation_right_hidden').style.visibility = 'hidden';
-         equation.querySelector('.equation_label').style.visibility = 'visible';
-		e.target.closest(".equation_right").style.backgroundColor = "transparent";
-	}
+$(".equation_left").mouseleave(function(){
+    $(this).parent().siblings(".equation_left_hidden").css("visibility", "hidden");
+    $(this).css("background-color", "transparent");
+});
+
+$(".equation_right").mouseleave(function(){
+    $(this).parent().siblings(".equation_right_hidden").css("visibility", "hidden");
+    $(this).css("background-color", "transparent");
+    $(this).parent().siblings(".equation_label").css("visibility", "visible");
 });
