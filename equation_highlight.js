8e1968ac031acbf308be2dc69313469055f94db9
ronangiss <46977169+ronangiss@users.noreply.github.com>
2024-03-24 17:31:58 -0400

    bernoulli's fixes

--- equation_highlight.js
+++ equation_highlight.js
@@ -1,35 +1,31 @@
 // Show hidden DIV on hover
 $(".equation_left").mouseenter(function(){
+    var termIndex = $(this).index();
     $(this).parent().siblings(".equation_left_hidden").css("visibility", "visible");
-    $(this).css("background-color", "lightgray");
-});
-
-$(".equation_middle").mouseenter(function(){
-    $(this).parent().siblings(".equation_left_hidden").css("display", "none");
-    $(this).parent().siblings(".equation_middle_hidden").css("display", "block");
+    $(this).parent().siblings(".equation_left_hidden").children().eq(termIndex).css("display", "block");
     $(this).css("background-color", "lightgray");
 });
 
 $(".equation_right").mouseenter(function(){
+    var termIndex = $(this).index() - $(this).parent().children().length;
     $(this).parent().siblings(".equation_right_hidden").css("visibility", "visible");
+    $(this).parent().siblings(".equation_right_hidden").children().eq(termIndex).css("display", "block");
     $(this).css("background-color", "lightgray");
     $(this).parent().siblings(".equation_label").css("visibility", "hidden");
 });
 
 // Hide DIV on mouse out
 $(".equation_left").mouseleave(function(){
+    var termIndex = $(this).index();
     $(this).parent().siblings(".equation_left_hidden").css("visibility", "hidden");
-    $(this).css("background-color", "transparent");
-});
-
-$(".equation_middle").mouseleave(function(){
-    $(this).parent().siblings(".equation_middle_hidden").css("display", "none");
-    $(this).parent().siblings(".equation_left_hidden").css("display", "block");
+    $(this).parent().siblings(".equation_left_hidden").children().eq(termIndex).css("display", "none");
     $(this).css("background-color", "transparent");
 });
 
 $(".equation_right").mouseleave(function(){
+    var termIndex = $(this).index() - $(this).parent().children().length;
     $(this).parent().siblings(".equation_right_hidden").css("visibility", "hidden");
+    $(this).parent().siblings(".equation_right_hidden").children().eq(termIndex).css("display", "none");
     $(this).css("background-color", "transparent");
     $(this).parent().siblings(".equation_label").css("visibility", "visible");
 });
